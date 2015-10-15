1fad78654f25c8ea974d37a6a765c62903f3582e
hlin117 <hlin117@gmail.com>
2015-10-15 02:20:09 -0500

    NOTES: Added more TODOs, and added homework7_5 table

--- chap7-discussion/homework7_5.py
+++ chap7-discussion/homework7_5.py
@@ -48,11 +48,8 @@ def play_round(lands_on_table, next_card_ind):
         card_counts[0] -= 1
 
     # In this selection process, we are interested in picking the
-    # card of least cost per round. If lands_on_table == 0, we don't
-    # enter the for loop.
-    #
-    # TODO: CHANGE ME FOR YOUR EXPERIMENT! (If needed...)
-    for spell_value in range(1, lands_on_table + 1):
+    # most expensive card per round. (Yes, the range loop below is right.)
+    for spell_value in range(lands_on_table, -1, -1):
         if card_counts[spell_value] > 0:
             card_counts[spell_value] -= 1
             return spell_value, lands_on_table
@@ -79,37 +76,35 @@ The code is on page 216.
 start = time()
 n_experiments = 10  # He calls these "simulations"
 n_simulations = 1000  # He calls these "inner simulations"
-counts = np.zeros(n_experiments)
 
-for exp in range(n_experiments):
-    for sim in range(n_simulations):
+# 6 x 6 table of zeros. The first dimension (row) represents the
+# turn, indexed at 0. The second dimension (col) represents the
+# "spell value - 1". (For example, index 5 corresponds to a spell value of 6.)
+counts = np.zeros((6, 6))
 
-        deck = make_deck(lands=24, spell1=10, spell2=10, spell3=10, spell4=2,
-                         spell5=2, spell6=2)
+for sim in range(n_simulations):
 
-        hand = deck[:7]
-        card_counts = get_card_counts(hand)
+    deck = make_deck(lands=24, spell1=10, spell2=10, spell3=10, spell4=2,
+                     spell5=2, spell6=2)
 
-        # From here, it's a matter of playing the game. Limit ourselves
-        # to only 4 turns.
-        n_turns = 4
-        spells_played = list()
-        lands_on_table = 0
-        next_card_ind = 7
-        for turn in range(n_turns):
-            spell_value, lands_on_table \
-                    = play_round(lands_on_table, next_card_ind)
-            next_card_ind += 1
+    hand = deck[:7]
+    card_counts = get_card_counts(hand)
 
-            # TODO: CHANGE ME FOR YOUR EXPERIMENT! (If needed...)
-            spells_played.append(spell_value)
+    # From here, it's a matter of playing the game. Limit ourselves
+    # to only 4 turns.
+    n_turns = 6
+    spells_played = list()
+    lands_on_table = 0
+    next_card_ind = 7
+    for turn_ind in range(n_turns):
+        spell_value, lands_on_table \
+            = play_round(lands_on_table, next_card_ind)
+        next_card_ind += 1
 
-        # Now we check that we played a card on each of the turns.
-        # The boolean converts to an integer, because counts is a
-        # (numpy) array of integers.
-        #
-        # TODO: CHANGE ME FOR YOUR EXPERIMENT! (If needed...)
-        counts[exp] += int(all(spell_value > 0 for spell_value in spells_played))
+        if spell_value > 0:
+            spell_ind = spell_value - 1
+            counts[turn_ind][spell_ind] += 1
+            break
 
 probabilities = counts / n_simulations
 n_seconds = round(time() - start)
@@ -117,3 +112,4 @@ print("Probabilities: ".ljust(25) + str(probabilities))
 print("Mean: ".ljust(25) + str(round(probabilities.mean(), 4)))
 print("Standard deviation: ".ljust(25) + str(round(probabilities.std(), 4)))
 print("Number of seconds: ".ljust(25) + str(format(n_seconds)))
+
